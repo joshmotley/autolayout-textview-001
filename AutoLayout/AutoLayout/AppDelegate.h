638f126c4ad93a90ed4db4541362d510e1f609ae
joshmotley <joshkmotley@gmail.com>
2015-09-07 21:09:49 -0400

    portrait constraints only

---
+++ AutoLayout/AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Joshua Motley on 9/7/15.
+//  Copyright (c) 2015 Motley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Joshua Motley on 9/7/15.
+//  Copyright (c) 2015 Motley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
